01139537d5fed234944dbaee198ba89d5c204da0
oarit <oarit@VARDOS>
2025-09-14 23:30:01 +0200

    init--> first commit

---
+++ src/test/java/andreas/kafkis/eberle/jewelry/shop/Jewelry/shop/Backend/JewelryShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package andreas.kafkis.eberle.jewelry.shop.Jewelry.shop.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
